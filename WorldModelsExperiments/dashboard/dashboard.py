d54604d282009db88f4ae2067acfecf4293b6976
ugebn <ugebn@student.kit.edu>
2020-05-13 16:07:46 +0200

    basic button press to display image

--- WorldModelsExperiments/dashboard/dashboard.py
+++ WorldModelsExperiments/dashboard/dashboard.py
@@ -2,50 +2,59 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import base64
+import flask
+import os
+import gym
+from PIL import Image
 
-video_path = '/home/dakaun/PycharmProjects/world_model/WorldModelsExperiments/explain/obs_sequence.mp4'
+# video_path = '/home/student/Dropbox/MA/assets/obs_sequence_all.mp4'
 
 app = dash.Dash(__name__)
 server = app.server
 
-video = open(video_path,'rb').read()
-b64 = base64.b64encode(video)
+colors = {
+    'text': '#111111'
+}
 
-app.layout = html.Div(children=[
-  html.Video(
-    controls= True,
-    scr="https://www.w3schools.com/html/mov_bbb.mp4",
-    autoPlay= True)
-])
+# video = open(video_path, 'rb').read()
+# encoded_video = base64.b64encode(video)
 
+app.layout = html.Div(style={
+    'textAlign':'center'
+    },
+    children=[
+        html.H1(children='Breakout Word Model',
+                style={
+                    'textAlign': 'center',
+                    'color': colors['text']
+                }),
+        html.Div(children='Dashboard to display the world model of breakout.',
+                 style={
+                     'textAlign': 'center',
+                     'color': colors['text']
+                 }),
+        html.Button('Press to start Breakout',id='start_game', n_clicks=0),
+        html.Img(id='game_img')
+
+        #display_envimg(),
+        # html.Video(id='videoexample',
+        #         controls=True,
+        #         src='data:video/mp4;base64,{}'.format(encoded_video.decode()),
+        #         style={
+        #             'textAlign': 'center'
+        #         })
+    ])
+@app.callback(
+    dash.dependencies.Output('game_img', 'src'),
+    [dash.dependencies.Input('start_game', 'n_clicks')]
+)
+def run_game(buttonclick):
+    env = gym.make('Breakout-v0')
+    if buttonclick:
+        obs = env.reset()
+        img = Image.fromarray(obs)
+        return img
 
 
 if __name__ == '__main__':
-  app.run_server(debug=True, host='0.0.0.0', port=1880)
-
-'''
-video
-def embed_mp4(filename,video):",
-    "  \"\"\"Embeds an mp4 file in the notebook.\"\"\"\n",
-    "  video = open(filename,'rb').read()\n",
-    "  b64 = base64.b64encode(video)\n",
-    "  tag = ,
-    "  <video width=\"640\" height=\"480\" controls>\n",
-    "    <source src=\"data:video/mp4;base64,{0}\" type=\"video/mp4\">\n",
-    "  Your browser does not support the video tag.\n",
-    "  </video>.format(b64.decode())",
-    "\n",
-    "  return IPython.display.HTML(tag)"
-    (1)
-    app.layout = html.Div(children=[
-  html.H1(children='Breakout Worldmodel'),
-  html.Video(src='data:video/mp4;base64,{} type=\'video/mp4\''.format(b64.decode()))
-  )]
-    nicht funktioniert - kein Video angeszeigt
-    (2)
-    app.layout = html.Div(children=[
-  html.Video(scr="https://www.w3schools.com/html/mov_bbb.mp4")
-])
-  (3)
-  
-'''
+    app.run_server(debug=True, host='0.0.0.0', port=1880)
