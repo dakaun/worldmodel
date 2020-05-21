1593ad6dd805d7e8a35f4d3a649a52ab8d65fa45
ugebn <ugebn@student.kit.edu>
2020-05-21 15:19:05 +0200

    display different options after pausing on dashboard - smoothly working

--- WorldModelsExperiments/dashboarddash/dashboard.py
+++ WorldModelsExperiments/dashboarddash/dashboard.py
@@ -3,11 +3,16 @@ import dash_html_components as html
 import base64
 from PIL import Image
 from WorldModelsExperiments.breakout.model import Model, make_model, _process_frame
+from WorldModelsExperiments.breakout.rnn.rnn import rnn_next_state
 import pandas as pd
 import numpy as np
 from pyglet.window import key
 import time
 import imageio
+import cv2
+import gym
+#import WorldModelsExperiments.dashboarddash.dash_player as dash_player
+
 
 path = '/home/student/Dropbox/MA/worldmodel/worldmodel-breakout-server-version-v3/200423'
 rnn_path = path + '/tf_rnn/rnn.json'
@@ -74,18 +79,45 @@ def play_game(model, num_episode=1, render_mode=True):
                 print('close env')
             return obs_sequence, seq_counter
         time.sleep(2)
-    return obs_sequence, seq_counter
+    return obs_sequence, seq_counter, obs, model.state, total_reward, model.env.clone_full_state(),z
 
+def resume_game(pause_status, action):
+    obs_normal = np.zeros(shape=(3000, 210, 160, 3), dtype=np.uint8)
+    obs_normal[:pause_status['counter']] = pause_status['sequence']
+    seq_counter = pause_status['counter'] + 10 # todo add white pages instead of black - more obvious
+    total_reward = pause_status['totalreward']
+    obs = pause_status['obs']
+    model.state = pause_status['modelstate']
+    model.env.restore_full_state(pause_status['gamestate'])
+    done = False
 
-def generate_video(sequence, filename, counter):
-    print('generating video..')
-    print(filename)
-    print(counter)
-    with imageio.get_writer(filename, mode='I', macro_block_size=None) as video:
-        for image in range(counter):
-            video.append_data(sequence[image])
-    print('done generating video')
+    if action==2 | action==3:
+        for i in range(1):
+            obs, _,_,_= model.env.step(action)
+            obs_into_z= _process_frame(obs)
+            z = model.vae.encode(obs_into_z.reshape(1,64,64,3))
+            action_one_hot = np.zeros(model.num_actions)
+            action_one_hot[action]=1
+            model.state = rnn_next_state(model.rnn, z, action_one_hot, model.state)
 
+    obs_normal[seq_counter,:,:,:]=obs
+    seq_counter+=1
+    while not done and seq_counter < 3000:
+        obs = _process_frame(obs)
+        z, mu, logvar = model.encode_obs(obs)
+        _, action = model.get_action(z)
+        obs, reward, done, info = model.env.step(action)
+        model.env.render('rgb_array')
+
+        obs_normal[seq_counter, :, :, :] = obs
+        total_reward += reward
+        seq_counter += 1
+
+    print('Episode is done with total reward: ', total_reward)
+    #model.env.viewer.close()
+    #model.env.viewer = None
+
+    return obs_normal,seq_counter
 
 app = dash.Dash(__name__)
 server = app.server
@@ -121,15 +153,10 @@ app.layout = html.Div(id='header1',
                                      controls=True,
                                      style={
                                          'textAlign': 'center'
-                                     })
-
-                          # display_video()
-                          # html.Video(id='videoexample',
-                          #         controls=True,
-                          #         src='data:video/mp4;base64,{}'.format(encoded_video.decode()),
-                          #         style={
-                          #             'textAlign': 'center'
-                          #         })
+                                     },
+                                     height=252,
+                                     width=576
+                                     )
                       ])
 
 
@@ -138,26 +165,48 @@ app.layout = html.Div(id='header1',
 def start_game(buttonclick):
     if buttonclick:
         print('start playing game')
-        obs_sequence, seq_counter = play_game(model)
-        print(obs_sequence.shape)
-        print('ending game')
-        init_obs_seq_filename = 'obs.mp4'
-        print('generating video..')
-        # np.savez_compressed('test.npz', obs=obs_sequence)
-        print(seq_counter)
-        # with imageio.get_writer(init_obs_seq_filename, mode='I', macro_block_size=None) as video:
-        #    for image in range(10):
-        #        video.append_data(obs_sequence[image])
 
+        initial_obs_sequence, seq_counter, obs, state, treward, gamestate, z = play_game(model)
+        pause_status = {
+            'sequence': initial_obs_sequence[:seq_counter, :, :, :],
+            'counter': seq_counter,
+            'obs': obs,
+            'modelstate': state,
+            'totalreward': treward,
+            'gamestate': gamestate,
+            'z': z
+        }
+        # normal
+        resume_obs_sequence_normal, seq_countern = resume_game(pause_status, 0)
+        print('normalgamedone')
+        # right
+        resume_obs_sequence_right, seq_counterr = resume_game(pause_status, 2)
+        print('rightdone')
+        # left
+        resume_obs_sequence_left, seq_counterl = resume_game(pause_status, 3)
+        print('leftdone')
+        all_images = np.concatenate((resume_obs_sequence_left, resume_obs_sequence_normal, resume_obs_sequence_right),
+                                    axis=2)
+        print(all_images.shape)
+
+        fin_counter = max(seq_counterl, seq_countern, seq_counterr)
+        init_obs_seq_filename = 'obs_video.webm'
+
+        height = all_images[0].shape[0]
+        width = all_images[0].shape[1]
+        sequence = all_images[:, :, :, [2, 1, 0]]
+
+        video = cv2.VideoWriter(init_obs_seq_filename, cv2.VideoWriter_fourcc(*'vp80'), 10, frameSize=(width, height))
+        for image in range(fin_counter):
+            video.write(sequence[image])
+        video.release()
         print('done generating video')
-        # generate_video(obs_sequence, init_obs_seq_filename, seq_counter)
-        print('loading video')
-        # filename = ''
-        video = open('/home/student/Dropbox/MA/assets/obs_sequence_all.mp4', 'rb').read()
-        encoded_video = base64.b64encode(video)
-        print('send video to dashboard')
-        return 'data:video/mp4;base64,{}'.format(encoded_video.decode())
 
+        videom = open(init_obs_seq_filename, 'rb').read()
+        encoded_video = base64.b64encode(videom).decode()
+        print('send video to dashboard')
+        print('readytorender')
+        return 'data:video/webm;base64,{}'.format(encoded_video)
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=1885)
+    app.run_server(debug=True, host='0.0.0.0', port=1873)
