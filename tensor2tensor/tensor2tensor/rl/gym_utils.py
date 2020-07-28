09e301eaffbca72ed1224955f0b0a5bd373d11e2
ugebn <ugebn@student.kit.edu>
2020-07-28 14:40:45 +0200

    adapt to gym

--- tensor2tensor/tensor2tensor/rl/gym_utils.py
+++ tensor2tensor/tensor2tensor/rl/gym_utils.py
@@ -19,6 +19,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
+sys.path.append('../../../gym')
 import gym
 import numpy as np
 import tensorflow as tf
